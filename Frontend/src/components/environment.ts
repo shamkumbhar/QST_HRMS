bffe3c58accc703736ee730b350150796d685806
shamkumbhar <shamkumbhar47@gmail.com>
2025-07-11 17:58:34 +0530

    Update environment.ts

--- Frontend/src/components/environment.ts
+++ Frontend/src/components/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,  // ✅ Since you're deploying this
-  apiBaseUrl: 'http://35.154.42.153:8080/api',  // 🔁 Replace with your backend IP/domain
+  apiBaseUrl: 'http://13.53.234.209:8080/api',  // 🔁 Replace with your backend IP/domain
   appName: 'HRMS',
   enableDebug: false
 };
